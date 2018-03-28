0ba3e7f8e74e8c5d5db1d03429beaf497f4524b6
rc46307 <35606001+rc46307@users.noreply.github.com>
2018-03-28 19:53:04 -0400

    first commit

---
+++ test.py
@@ -0,0 +1,43 @@
+import pygame
+import random
+from Classes import *
+
+def main():
+    print("####### testing player model######")
+
+    test_player = Player()
+    #print(test_player.rect.center)
+
+
+    print("=====Standard Input Test=====")
+
+    test_player.move(pygame.K_UP)
+    assert test_player.getCoordinates() == (0,-5)
+
+    test_player = Player()
+    #print(test_player.rect.center)
+    print("=====Standard Input Test=====")
+
+    test_player.move(pygame.K_DOWN)
+    assert test_player.getCoordinates() == (0,5)
+
+    test_player = Player()
+    #print(test_player.rect.center)
+    print("=====Standard Input Test=====")
+
+    test_player.move(pygame.K_LEFT)
+    assert test_player.getCoordinates() == (-5,0)
+
+    test_player = Player()
+    #print(test_player.rect.center)
+    print("=====Standard Input Test=====")
+
+    test_player.move(pygame.K_RIGHT)
+    assert test_player.getCoordinates() == (5,0)
+
+    print("######## player test complete#######")
+
+    
+
+
+main()
